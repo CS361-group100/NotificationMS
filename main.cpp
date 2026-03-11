24fe1f185a512c4cf41946eb8de37ba6c7bd4f6e
Rachel Luginbill <rachelluginbill27@gmail.com>
2026-03-11 12:52:27 -0700

    Created initial files and comments.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*@Author: Rachel Luginbill
+@Date: March 11, 2026
+@Title: Notification Microservice
+@Description: This Microservice will require asking user for their email,
+ then you have to send-through JSON-a txt file that holds the contents of the 
+ email for the microservice to then format and send to the users provided email.
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+std::string usersEmail;
+
+int main(){
+    //assign given email to usersEmail
+    
+    //retrieve text file and check to make sure it is not empty return 0 if empty
+
+    //send the text file contents as the email
+
+
+}
